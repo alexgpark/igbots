bf9f77348d638c4c6621c66b8bf35e2d7fb00f26
alexgpark <alexgpark@gmail.com>
2018-01-23 22:48:29 -0800

    fix hellojs

--- hello.js
+++ hello.js
@@ -2,11 +2,12 @@ console.log("hello");
 
 const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://trends.google.com/trends/explore?date=now 7-d&q=XRP');
-  await page.screenshot({path: 'test.png'});
+const browser = await puppeteer.launch();
 
-  await browser.close();
-})();
+const page = await browser.newPage();
+await page.goto('https://trends.google.com/trends/explore?date=now 7-d&q=ethereum');
+
+console.log(await page.content());
+await page.screenshot({path: 'screenshot.png'});
+
+await browser.close();
